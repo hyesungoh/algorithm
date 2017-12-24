20b0be6ab545d9792ea9d914f7cee56d78ac86ff
haesungoh414 <haesungoh414@gmail.com>
2017-12-25 03:14:09 +0900

    12.25.2017
    그룹 단어 체크

---
+++ groupWord.py
@@ -0,0 +1,13 @@
+# 이름 : 오혜성
+# 날짜 : 12 25 2017
+# 주제 : 그룹 단어 체크
+
+m = 0; n = int(input())
+for i in range(n):
+    l = []
+    for i in input():
+        if i not in l:
+            l.append(i)
+        elif l[len(l)-1] != i:
+            m+=1
+print(n-m)
