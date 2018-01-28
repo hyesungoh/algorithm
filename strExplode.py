76a6be91a000d0187ffc8a68d20df7f3f8e9f989
haesungoh414 <haesungoh414@gmail.com>
2018-01-29 02:07:47 +0900

    01.29.2018
    문자열 폭발 시간초과 개선

--- strExplode.py
+++ strExplode.py
@@ -9,12 +9,27 @@
 # print("FRULA" if s == "" else s)
 
 # 파이썬에서 제일 빠른 Replace, 정규표현식의 sub를 사용해도 시간초과 발생
-from re import sub as sub
-s = input(); e = input()
-while 1:
-    t = s
-    s = sub(e,"",s)
-    if t == s: break
-    # if e in s: s = sub(e, "", s)
-    # else: break
-print("FRULA" if s == "" else s)
+# from re import sub as sub
+# s = input(); e = input()
+# while 1:
+#     t = s
+#     s = sub(e,"",s)
+#     if t == s: break
+#     # if e in s: s = sub(e, "", s)
+#     # else: break
+# print("FRULA" if s == "" else s)
+
+# s = list(input()); b = list(input())
+# bSize = len(b); b.reverse(); ans = []
+# for i in s:
+#     ans.append(i)
+#     if list(reversed(ans))[0:bSize] == b:
+#         for _ in range(bSize): ans.pop()
+# print("FRULA" if ans == [] else "".join(ans))
+
+n = list(input()); b = input(); bS = len(b); s = ""
+for i in n:
+    s += i
+    if(s[-bS:] == b):
+        s = s[:-bS]
+print("FRULA" if not s else s)
